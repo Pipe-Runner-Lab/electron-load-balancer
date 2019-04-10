0e22f12c62e60ce03154efe77c616d8db21947a5
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-04-11 00:32:01 +0530

    functions dedicated to main process complete, bussiness logic left

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { ipcMain, ipcRenderer, BrowserWindow } = require('electron');
+const { BrowserWindow } = require('electron');
 const cpus = require('os').cpus().length;
 const path = require('path');
 const url = require('url');
 
-let registeredPath = [];
-let workerList = []; 
+let registeredPath = {};
+let workerList = {}; 
 
 const createHiddenWindow = ( filePath ) => {
 	const startUrl = url.format({
@@ -19,36 +19,73 @@ const createHiddenWindow = ( filePath ) => {
 	});
 }
 
-ipcMain.on( "START_BACKGROUND_PROCESS", (event, args) => {
-
-} )
-
 exports.printMsg = () => {
 	console.log("This is a message from the demo package");
 }
 
-exports.startBackgroundProcess = ( processName, args ) => {
+exports.startBackgroundProcess = ( ipcRenderer ,processName, args ) => {
 	/* 
-	Desciption: Takes an array of objects with each object
-	having a key value pair of 'name':'filepath'
+	Desciption: Starts the background prcoess matching the
+	name from the registeredpath array
 	*/
-	ipcRenderer.send("START_BACKGROUND_PROCESS", args)
+	ipcRenderer.send("BACKGROUND_PROCESS_START", args)
 }
 
-exports.register = ( args ) => {
+exports.register = ( ipcMain, args ) => {
 	/* 
 	Desciption: Takes an array of objects with each object
 	having a key value pair of 'processName':'filepath'
 	*/
+	
 	registeredPath = args;
+	ipcMain.on( "BACKGROUND_PROCESS_START", (event, args) => {
+		if( workerList.length < cpus ){
+			const { processName, values } = args;
+			workerList = 
+			{ ...workerList, 
+				[processName]: 
+				{
+					windowObject: createHiddenWindow( registeredPath[processName] ),
+					values
+				} 
+			};
+		}
+		else{
+			console.log( 'Out of CPU' );
+		}
+	} );
+
+	ipcMain.on( "BACKGROUND_PROCESS_INITIALIZED", (event, args) => {
+		// use this to pass the initialization data
+		// send back initialization data
+		const { processName } = args;
+		event.send( 'BACKGROUND_PROCESS_WORKING', {
+			values: workerList[processName].values
+		})
+	} );
+
+	ipcMain.on( "BACKGROUND_PROCESS_FINISHED", (event, args) => {
+		// use this to pass the initialization data
+		const { processName } = args;
+		delete workerList[processName];
+	} );
 }
 
-exports.initialize = ( args ) => {
+exports.initialize = ( ipcRenderer, processName, func ) => {
 	/* 
-	Desciption: Takes an array of objects with each object
-	having a key value pair of 'processName':'filepath'
+	Desciption: Sends an ipc confirmation ensuring that
+	the frameless window has been created. Can be used 
+	send initialization data
 	*/
-	registeredPath = args;
+
+	ipcRenderer.send( 'BACKGROUND_PROCESS_INITIALIZED', {
+		processName
+	} );
+
+	ipcRenderer.on( 'BACKGROUND_PROCESS_WORKING', ( event, args ) => {
+		const { values } = args;
+		func( values );
+	} )
 }
 
 exports.kill = ( args ) => {
