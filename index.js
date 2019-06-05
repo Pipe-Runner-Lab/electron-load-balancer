8d3a45b76ee690053578fe31bce1e9071a4712c4
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-06-05 15:59:47 +0530

    structural changes and kill all process feature added

--- index.js
+++ index.js
@@ -3,112 +3,113 @@ const cpus = require('os').cpus().length;
 const path = require('path');
 const url = require('url');
 
-let registeredPaths = {};
+let globalRegisteredPaths = {};
 let workers = {};
 
 const createHiddenWindow = filePath => {
-	const startUrl = url.format({
-		pathname: path.join(__dirname, `/../../${filePath}`),
-		protocol: 'file:',
-		slashes: true,
-	});
-	let hiddenWindow = new BrowserWindow({ show: false });
-	hiddenWindow.loadURL(startUrl);
-	hiddenWindow.on('closed', () => {});
-	return hiddenWindow;
+  const startUrl = url.format({
+    pathname: path.join(__dirname, `/../../${filePath}`),
+    protocol: 'file:',
+    slashes: true,
+  });
+  let hiddenWindow = new BrowserWindow({ show: false });
+  hiddenWindow.loadURL(startUrl);
+  hiddenWindow.on('closed', () => {});
+  return hiddenWindow;
 };
 
 exports.register = (ipcMain, registeredPaths) => {
-	registeredPaths = registeredPaths;
-	ipcMain.on('BACKGROUND_PROCESS_START', (event, args) => {
-		if (Object.keys(workers).length < cpus) {
-			const { processName, values } = args;
-			workers = {
-				...workers,
-				[processName]: {
-					windowObject: createHiddenWindow(registeredPaths[processName]),
-					values,
-				},
-			};
-		} else {
-			console.log('Out of CPUs');
-		}
-	});
+  globalRegisteredPaths = registeredPaths;
 
-	ipcMain.on('BACKGROUND_PROCESS_STOP', (event, args) => {
-		const { processName } = args;
-		workers[processName] &&
-			workers[processName].windowObject.webContents.send('BACKGROUND_PROCESS_STOP');
-	}); 
+  ipcMain.on('BACKGROUND_PROCESS_START', (event, args) => {
+    if (Object.keys(workers).length < cpus) {
+      const { processName, values } = args;
+      workers = {
+        ...workers,
+        [processName]: {
+          windowObject: createHiddenWindow(globalRegisteredPaths[processName]),
+          values,
+        },
+      };
+    } else {
+      console.log('Out of CPUs');
+    }
+  });
 
-	ipcMain.on('WORKER_INIT', (event, args) => {
+  ipcMain.on('BACKGROUND_PROCESS_STOP', (event, args) => {
 		const { processName } = args;
-		event.sender.send('WORKER_INIT_COMPLETE', {
-			values: workers[processName].values,
-		});
-	});
+    workers[processName].windowObject.webContents.send('WORKER_KILL');
+    workers[processName].windowObject.close();
+    delete workers[processName];
+  });
 
-	ipcMain.on('WORKER_KILL', (event, args) => {
-		const { processName } = args;
-		workers[processName].windowObject.close();
-		delete workers[processName];
-	});
+  ipcMain.on('WORKER_INIT', (event, args) => {
+    const { processName, killfunc } = args;
+    workers = {
+      ...workers,
+      [processName]: {
+        ...workers[processName],
+        killfunc,
+      },
+    };
+    event.sender.send('WORKER_INIT_COMPLETE', {
+      values: workers[processName].values,
+    });
+  });
 
-	ipcMain.on('TO_WORKER', (event, args) => {
-		const { processName } = args;
-		workers[processName].windowObject.webContents.send('TO_WORKER', args);
-	});
+  ipcMain.on('TO_WORKER', (event, args) => {
+    const { processName } = args;
+    workers[processName].windowObject.webContents.send('TO_WORKER', args);
+  });
+};
+
+exports.stopAllBackgroundProcess = () => {
+  Object.keys(workers).map(processName => {
+    workers[processName].windowObject.webContents.send('WORKER_KILL');
+    workers[processName].windowObject.close();
+    delete workers[processName];
+  });
 };
 
 exports.startBackgroundProcess = (ipcRenderer, processName, values) => {
-	ipcRenderer.send('BACKGROUND_PROCESS_START', {
-		processName,
-		values,
-	});
+  ipcRenderer.send('BACKGROUND_PROCESS_START', {
+    processName,
+    values,
+  });
 };
 
 exports.stopBackgroundProcess = (ipcRenderer, processName) => {
-	ipcRenderer.send('BACKGROUND_PROCESS_STOP', {
-		processName,
-	});
+  ipcRenderer.send('BACKGROUND_PROCESS_STOP', {
+    processName,
+  });
 };
 
-exports.onInitialize = (ipcRenderer, processName, func) => {
-	ipcRenderer.on('WORKER_INIT_COMPLETE', (event, args) => {
-		const { values } = args;
-		func(values);
-	});
+exports.onInitialize = (ipcRenderer, processName, func, killfunc) => {
+  ipcRenderer.on('WORKER_INIT_COMPLETE', (event, args) => {
+    const { values } = args;
+    func(values);
+  });
 
-	ipcRenderer.send('WORKER_INIT', {
-		processName,
-	});
-};
+  ipcRenderer.on('WORKER_KILL', (event, args) => {
+    killfunc && killfunc();
+  });
 
-exports.onFinish = (ipcRenderer, processName, func) => {
-	ipcRenderer.on('BACKGROUND_PROCESS_STOP', (event, args) => {
-		if (func) func();
-		ipcRenderer.send('WORKER_KILL', {
-			processName,
-		});
-	});
-};
-
-exports.finish = (ipcRenderer, processName) => {
-	ipcRenderer.send('WORKER_KILL', {
-		processName,
-	});
+  ipcRenderer.send('WORKER_INIT', {
+    processName,
+  });
 };
 
 exports.send = (ipcRenderer, processName, values) => {
-	ipcRenderer.send('TO_WORKER', {
-		processName,
-		values,
-	});
+  ipcRenderer.send('TO_WORKER', {
+    processName,
+    values,
+  });
 };
 
 exports.on = (ipcRenderer, processNameOuter, func) => {
-	ipcRenderer.on('TO_WORKER', (event, args) => {
-		const { processName, values } = args;
-		processNameOuter === processName && func(values);
-	});
+  ipcRenderer.on('TO_WORKER', (event, args) => {
+    const { processName, values } = args;
+    processNameOuter === processName && func(values);
+  });
 };
+
