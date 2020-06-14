64003c198cffe4ddde2bc88d999b374afd6bd716
Daniel Maslowski <info@orangecms.org>
2020-06-14 14:24:57 +0200

    Set `nodeIntegration: true for >= Electron v5
    Also add a `debug` flag to show the otherwise hidden window.

--- index.js
+++ index.js
@@ -6,28 +6,39 @@ const url = require('url');
 let globalRegisteredPaths = {};
 let workers = {};
 
-const createHiddenWindow = filePath => {
+const createHiddenWindow = (filePath, debug) => {
   const startUrl = url.format({
     pathname: path.join(__dirname, `/../../${filePath}`),
     protocol: 'file:',
     slashes: true,
   });
-  let hiddenWindow = new BrowserWindow({ show: false });
+  let hiddenWindow = new BrowserWindow({
+    // This is essential for this library to work, so that the new
+    // window can use `require`. See also
+    // https://github.com/electron/electron/issues/17241.
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    // This is extremely useful when something with the IPC goes
+    // wrong.
+    show: debug,
+  });
   hiddenWindow.loadURL(startUrl);
   hiddenWindow.on('closed', () => {});
   return hiddenWindow;
 };
 
-exports.register = (ipcMain, registeredPaths) => {
+exports.register = (ipcMain, registeredPaths, { debug = false }) => {
   globalRegisteredPaths = registeredPaths;
 
   ipcMain.on('BACKGROUND_PROCESS_START', (event, args) => {
     if (Object.keys(workers).length < cpus) {
       const { processName, values } = args;
+      const windowObject = createHiddenWindow(globalRegisteredPaths[processName], debug);
       workers = {
         ...workers,
         [processName]: {
-          windowObject: createHiddenWindow(globalRegisteredPaths[processName]),
+          windowObject,
           values,
         },
       };
@@ -63,6 +74,7 @@ exports.register = (ipcMain, registeredPaths) => {
   });
 };
 
+// FIXME: object is destroyed beforehand, causing an error here
 exports.stopAll = () => {
   Object.keys(workers).map(processName => {
     workers[processName].windowObject.webContents.send('WORKER_KILL');
